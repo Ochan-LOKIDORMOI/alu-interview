fb616c6ccc847235e8bd567d3d3b78bb46592e2b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-09-15 02:36:32 -0700

    Minimum Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,17 +6,11 @@ def minOperations(n):
     """
     Returns the operations
     """
-    if n <= 1:
-        return n
-
-    operations = 0
-    current_length = 1
-    clipboard = 0
-
-    while current_length < n:
-        if n % current_length == 0:
-            clipboard = current_length
-        operations += 1
-        current_length += clipboard
-
-    return operations
+     a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+        b += 1
+    return a
