24bf592f5d44e3687248a20a0eab2f14a8227c0b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-09-15 02:21:16 -0700

    Minimum Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
 
+"""
+Module to calculate the minimum number of operations (paste) needed to reach a target number of H characters in a file.
+
+The file initially contains a single H character. Each paste operation doubles the current number of H's.
+
+This module contains one function:
+
+minOperations(n)
+    - Calculates the minimum number of paste operations needed to reach a target of n H characters
+    - Returns an integer
+    - If the target n is not possible to reach, returns 0
+"""
+
+
 def minOperations(n):
+    """
+    Calculate minimum operations to reach target number of H characters.
+
+    Args:
+        n (int): Target number of H characters
+
+    Returns:
+        int: Minimum number of operations
+        0 if target is not possible
+    """
+
     if n < 1:
         return 0
 
