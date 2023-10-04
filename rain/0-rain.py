95098583af7e1fac2c8e786f12764bdf9f7aad7c
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-10-04 14:54:14 -0700

    Rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-
 """
 Module to calculate amount of water retained after rain on walls.
-
-This module contains one function:
 
 rain(walls)
   - Calculate total amount of water retained given heights of walls
   - walls is a list of non-negative integers representing wall heights
   - Returns an integer
-"""
+  """
 
 
 def rain(walls):
-
-  """
-  Calculate total amount of water retained.
+    """
+  Calculate total amount of water retained.  
 
   Args:
     walls (list): List of non-negative integers representing wall heights
@@ -24,18 +20,17 @@ def rain(walls):
     int: Total amount of water retained
 
   """
+    if not walls or len(walls) < 3:
+        return 0
 
-  if not walls:
-    return 0
+    water = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
 
-  total_water = 0
-  left = 0
-  right = len(walls) - 1
-  left_max = walls[left]
-  right_max = walls[right]
-
-  while left < right:
-
-    # calculate trapped water on smaller side
-
-    return total_water
+    return water
