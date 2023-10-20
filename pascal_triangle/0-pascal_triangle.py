8e88755c3ce70da127145cbf7f1ccd699831fa10
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-10-20 09:39:54 +0000

    Pascal Triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,12 +3,9 @@
 Pascal triangle
 """
 
-
-"""
-a function def pascal_triangle(n): 
-that returns a list of lists of integers representing the Pascal
-triangle of n
-"""
+ """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal triangle of n
+    """
 def pascal_triangle(n):
     if n <= 0:
         return []
